b64217dd3d445afc79b5feb0fae9407551be044f
OH-JOO-YEONG <brb1111@naver.com>
2022-01-05 22:53:00 +0900

    [Chore] 폴더 구조 및 리액트 라우터, 코드 스플리팅 이모션

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { FC } from 'react';
+import loadable from '@loadable/component';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+const Login = loadable(() => import('@pages/Login'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
-  return <div>좀만 참아주세요!!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
