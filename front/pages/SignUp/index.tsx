7540148340b08e3c59de3cc2af335de1b148c1e2
OH-JOO-YEONG <brb1111@naver.com>
2022-01-13 17:42:58 +0900

    [Feat] 로그인 페이지 구현

--- front/pages/SignUp/index.tsx
+++ front/pages/SignUp/index.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from 'react';
 import { Button, Error, Form, Header, Input, Label, LinkContainer, Success } from '@pages/SignUp/styles';
 import useInput from '@hooks/useInput';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   const [email, onChangeEmail] = useInput('');
@@ -96,7 +97,7 @@ const SignUp = () => {
       </Form>
       <LinkContainer>
         이미 회원이신가요?&nbsp;
-        <a href="/login">로그인 하러가기</a>
+        <Link to="/login">로그인 하러가기</Link>
       </LinkContainer>
     </div>
   );
