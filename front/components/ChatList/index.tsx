870c32630f891ef2c7f7e60618c18d3cc52c759b
unknown <brb1111@naver.com>
2022-01-28 22:45:42 +0900

    [Feat] DM 페이지 구현

---
+++ front/components/ChatList/index.tsx
@@ -0,0 +1,11 @@
+// import Chat from '@components/Chat';
+import { ChatZone, Section, StickyHeader } from '@components/ChatList/styles';
+import { IChat, IDM } from '@typings/db';
+import React, { FC, RefObject, useCallback } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars-2';
+
+const ChatList: FC = () => {
+  return <ChatZone></ChatZone>;
+};
+
+export default ChatList;
--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Container, Header } from './styles';
+import gravatar from 'gravatar';
+import useSWR from 'swr';
+import { IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import { useParams } from 'react-router';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
 
 const DirectMessage = () => {
-  return <div>로그인하신 것을 축하드려요</div>;
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+
+  const { data: myData } = useSWR('/api/users', fetcher);
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+
+  if (!userData || !myData) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatList />
+      <ChatBox chat="" />
+    </Container>
+  );
 };
 
 export default DirectMessage;
